aad6f3062432efdffb1954e29f075616b6a3d146
Irakli <i.botchori@gmail.com>
2022-07-23 21:40:57 +0400

    wip: add image upload feature

--- src/routes/bandMemberRoutes.ts
+++ src/routes/bandMemberRoutes.ts
@@ -1,8 +1,20 @@
 import express from 'express'
 import { bandMemberRegister } from 'controllers/bandMemberController'
+import { multerConfig } from '../config/multer'
+import multer from 'multer'
+import { Request, Response } from 'express'
 
 const router = express.Router()
 
 router.post('/register', bandMemberRegister)
+router.post(
+  '/change-avatar',
+  multer(multerConfig).single('image'),
+  (request: Request, response: Response) => {
+    console.log(request.file)
+
+    return response.json({ message: 'Image uploaded' })
+  }
+)
 
 export default router
