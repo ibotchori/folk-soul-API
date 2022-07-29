2ca4bd6fd955c8954c30feed65368ac0429de137
Irakli <i.botchori@gmail.com>
2022-07-29 16:11:28 +0400

    feat: add authMiddleware on social link routes

--- src/routes/socialLinkRoutes.ts
+++ src/routes/socialLinkRoutes.ts
@@ -6,21 +6,23 @@ import {
   registerSocialLink,
   updateSocialLink,
   getSocialLink,
-} from 'controllers/socialLinkCOntroller'
+} from 'controllers/socialLinkController'
 import { multerConfig } from '../config/multer'
 import multer from 'multer'
+import authMiddleware from 'middleware/authMiddleware'
 
 const router = express.Router()
 
-router.post('/register', registerSocialLink)
-router.put('/update/:id', updateSocialLink)
+router.post('/register', authMiddleware, registerSocialLink)
+router.put('/update/:id', authMiddleware, updateSocialLink)
 router.post(
   '/change-avatar/:id',
   multer(multerConfig).single('image'),
+  authMiddleware,
   changeSocialLinkAvatar
 )
-router.delete('/delete/:id', deleteSocialLink)
-router.get('/get/:id', getSocialLink)
+router.delete('/delete/:id', authMiddleware, deleteSocialLink)
+router.get('/get/:id', authMiddleware, getSocialLink)
 router.get('/get-all', getAllSocialLinks)
 
 export default router
